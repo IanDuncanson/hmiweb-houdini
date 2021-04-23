c59e48f19c4ddbabd102fc53967110f8c44e431e
Ian Duncanson <Ian.Duncanson@honeywell.com>
2021-04-23 16:43:27 +1000

    Change to vector drawing

--- rect.js
+++ rect.js
@@ -1,71 +1,38 @@
-registerPaint('hollowHighlights', class {
-
-  static get inputProperties() { return ['--boxColor']; }
-  // Input arguments that can be passed to the `paint` function
-  static get inputArguments() { return ['*','<length>']; }
-
-  static get contextOptions() { return {alpha: true}; }
-
-  paint(ctx, size, props, args) {
-    // ctx   -> drawing context
-    // size  -> size of the box being painted
-    // props -> list of custom properties available to the element
-	// args  -> list of arguments set when calling the paint() function in the css
-
-		// where to start the highlight & dimensions
-		const x = 0;
-		const y = size.height * 0.3;
-		const blockWidth = size.width * 0.33;
-		const blockHeight = size.height * 0.85;
-
-		// the values passed in the paint() function in the CSS
-		const theColor = props.get( '--boxColor' );
-		const strokeType = args[0].toString();
-		const strokeWidth = parseInt(args[1]);
-
-		console.log(theColor);
-		
-		// set the stroke width
-		if ( strokeWidth ) {
-			ctx.lineWidth = strokeWidth;
-		} else {
-			ctx.lineWidth = 1.0;
-		}
-		// set the fill type
-		if ( strokeType === 'stroke' ) {
-			ctx.fillStyle = 'transparent';
-			ctx.strokeStyle = theColor;
-		} else if ( strokeType === 'filled' ) {
-			ctx.fillStyle = theColor;
-			ctx.strokeStyle = theColor;
-		} else {
-			ctx.fillStyle = 'none';
-			ctx.strokeStyle = 'none';
-		}
-
-		// block
-		ctx.beginPath();
-		ctx.moveTo( x, y );
-		ctx.lineTo( blockWidth, y );
-		ctx.lineTo( blockWidth + blockHeight, blockHeight );
-		ctx.lineTo( x, blockHeight );
-		ctx.lineTo( x, y );
-		ctx.closePath();
-		ctx.fill();
-		ctx.stroke();
-		// dashes
-		for (let i = 0; i < 4; i++) {
-			let start = i * 2;
-			ctx.beginPath();
-			ctx.moveTo( blockWidth + (start * 10) + 10, y);
-			ctx.lineTo( blockWidth + (start * 10) + 20, y);
-			ctx.lineTo( blockWidth + (start * 10) + 20 + blockHeight, blockHeight);
-			ctx.lineTo( blockWidth + (start * 10) + 10 + blockHeight, blockHeight);
-			ctx.lineTo( blockWidth + (start * 10) + 10, y);
-			ctx.closePath();
-			ctx.fill();
-			ctx.stroke();
-		}
-
-  } // paint
-});
+registerPaint('svg-in-css', class {
+    static get inputProperties() {
+        return [
+          '--svg-viewbox',
+          '--svg-path',
+          '--svg-fill',
+          '--svg-stroke',
+          '--svg-stroke-width'
+        ];
+    }
+    paint(ctx, geom, properties) {
+        let viewbox = String(properties.get('--svg-viewbox')).trim() || '0 0 100 100';
+        viewbox = viewbox.split(' ').map(Number);
+  
+        let path = String(properties.get('--svg-path')).trim() || 'path("M0,0")';
+        // get only SVG path values :(
+        // = everything between quotes
+        let search = /['"`]([^'"`]+)/;
+        path = path.match(search)[1].trim();
+  
+        let fill = String(properties.get('--svg-fill'));
+        let stroke = String(properties.get('--svg-stroke'));
+        let strokeWidth = String(properties.get('--svg-stroke-width'));
+  
+        // adapt viewbox to canvas
+        ctx.scale(geom.width / viewbox[2], geom.height / viewbox[3]);
+        ctx.translate(-viewbox[0], -viewbox[1]);
+  
+        let path2D = new Path2D(path);
+  
+        ctx.fillStyle = fill;
+        ctx.fill(path2D);
+  
+        ctx.strokeStyle = stroke;
+        ctx.lineWidth = strokeWidth;
+        ctx.stroke(path2D);
+    }
+  })
